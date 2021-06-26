bbb377afa3082c2f85a60a980a07ce0fe9759c38
Winston Cahya <winstoncahya90@gmail.com>
2021-06-27 01:14:21 +0800

    second

--- controllers/login.js
+++ controllers/login.js
@@ -1,23 +1,23 @@
-const User = require('../models/User.js')
-const bcrypt = require('bcrypt')
+// const User = require('../models/User.js')
+// const bcrypt = require('bcrypt')
 
-const loginRouter = require('express').Router()
+// const loginRouter = require('express').Router()
 
-registerRouter.post('/', (request, response) => {
-    const body = request.body
+// registerRouter.post('/', (request, response) => {
+//     const body = request.body
     
   
-    if (body.username === undefined) {
-      return response.status(400).json({ error: 'username missing' })
-    }
+//     if (body.username === undefined) {
+//       return response.status(400).json({ error: 'username missing' })
+//     }
     
-  const verifyUser = User.findOne({username: body.username})
-  .then(
-      user => {
-          return user === null ? false : bcrypt.compare(body.password, user.password)
-      })
-      .then(verified => {
-          if(verified)
-      })
-  )
-  module.exports = loginRouter
+//   const verifyUser = User.findOne({username: body.username})
+//   .then(
+//       user => {
+//           return user === null ? false : bcrypt.compare(body.password, user.password)
+//       })
+//       .then(verified => {
+//           if(verified)
+//       })
+//   )
+//   module.exports = loginRouter
