8423b69e8c7a3ff44b25bb464dd7dc21fc09af48
Winston Cahya <winstoncahya90@gmail.com>
2021-06-27 01:38:33 +0800

    conenctedtoDb

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(express.static('build'))
 app.use(express.json())
 const registerRouters = require('./controllers/register')
 app.use('/api/users', registerRouters)
-
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
