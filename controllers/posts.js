a81e1acbf7b8fedb67a9650a52d50906fc49ffcf
Winston Cahya <winstoncahya90@gmail.com>
2021-06-28 23:08:39 +0800

    RegisterFixed

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,8 +2,7 @@ const Post = require('../models/Post')
 const User = require('../models/User')
 const postRoute = require('express').Router()
 const token = require('jsonwebtoken')
-require('dotenv').config()
-var bodyParser = require('body-parser');
+
 
 
 postRoute.get('/', (request, response) => {
@@ -14,37 +13,39 @@ postRoute.get('/', (request, response) => {
     
 })
 
-postRoute.delete('/:id', async (request, response) => {
-    const content = request.body
-    let decode
-    try{
-        decode = token.verify(content.logintoken, process.env.SECRET)
-    } catch {
-        return response.status(401).send({error: "Invalid token"})
-    }
+// postRoute.delete('/:id', async (request, response) => {
+//     const content = request.body
+//     let decode
+//     try{
+//         decode = token.verify(content.logintoken, process.env.SECRET)
+//     } catch {
+//         return response.status(401).send({error: "Invalid token"})
+//     }
     
-    const userDeleting = await Post.findOne({id: decode.id})
-    const deletePostId = request.params.id
-    const postDeleted = await Post.findOne({id: deletePostId})
+//     const userDeleting = await Post.findOne({id: decode.id})
+//     const deletePostId = request.params.id
+//     const postDeleted = await Post.findOne({id: deletePostId})
     
     
-    const deletion = await Post.deleteOne({id: deleteId}, err =>{
-        if(err){
-            return response.status(404).send({error: "an Error has occured, user may not be found"})
-        } else {
-            return response.status(200).send({success: "succesfully removed a post"})
-        }
-    } )
+//     const deletion = await Post.deleteOne({id: deleteId}, err =>{
+//         if(err){
+//             return response.status(404).send({error: "an Error has occured, user may not be found"})
+//         } else {
+//             return response.status(200).send({success: "succesfully removed a post"})
+//         }
+//     } )
 
-})
-postRoute.put('/:id', (request, response) => {
-    //tobe completed
-    // const content = request.body
-    // const newTextContent = content.content
-    // const newImage = content.image
+// })
+// postRoute.put('/:id', (request, response) => {
+
+//     //tobe completed
+//     // const content = request.body
+//     // const newTextContent = content.content
+//     // const newImage = content.image
     
     
-})
+
+// })
 postRoute.post('/', async (request, response) => {
     const content = request.body
     let decode
@@ -65,5 +66,6 @@ postRoute.post('/', async (request, response) => {
         const newPost = await Post.create(content)
         return response.status(200)
     }
+
 })
 module.exports = postRoute
