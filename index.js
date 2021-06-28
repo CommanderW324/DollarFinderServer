f0db93c721942928f2018dcaa752cfc446d9ae80
Winston Cahya <winstoncahya90@gmail.com>
2021-06-28 20:19:32 +0800

    TryImage

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express()
 const cors = require('cors')
 const router = require('./router')
 const bodyParser = require('body-parser');
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 const registerRoutes = require('./controllers/register')
 const loginRoutes = require('./controllers/login')
 const postRoutes = require('./controllers/posts')
