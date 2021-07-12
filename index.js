a53ac584205a66ddba36baf5c4f9f59a1ce88e1d
Winston Cahya <winstoncahya90@gmail.com>
2021-07-12 15:42:13 +0800

    Rewrote dolar

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const cors = require('cors')
 const router = require('./router')
 app.use(cors())
 app.use(express.static('build'))
-var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(express.json())
 
 const registerRoutes = require('./controllers/register')
